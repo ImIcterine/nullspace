ff15e66923ff2dfbe8f3e611d073597f53f02310
ImIcterine <113456363+ImIcterine@users.noreply.github.com>
2026-04-21 15:31:20 +0300

    Added v0.0.2 to root

--- script.js
+++ script.js
@@ -25,6 +25,10 @@ window.onload = async () => {
         return value.toLowerCase()
     }
 
+    function sharp(img) {
+        img.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
+    }
+
     function newTerminal(textObj) {
         if (!textObj || typeof textObj !== 'object') return null
 
@@ -132,6 +136,23 @@ window.onload = async () => {
         return terminal
     }
 
+    async function newUiBox(w, h) {
+        const boxTex = await PIXI.Assets.load(assetsPrefix + "nine-slice-box2.png")
+        
+        const box = new PIXI.NineSliceSprite({
+            texture: boxTex,
+            leftWidth: 5,
+            rightWidth: 5,
+            topHeight: 5,
+            bottomHeight: 5
+        })
+    
+        box.width = w
+        box.height = h
+
+        return box
+    }
+
     const assetsPrefix = "https://cdn.jsdelivr.net/gh/ImIcterine/nullspace@main/assets/pack0/"
 
     const cvs = document.getElementById("screen")
@@ -142,6 +163,8 @@ window.onload = async () => {
         height: cvs.height,
         backgroundColor: 0x000000
     })
+
+    const version = "v0.0.2"
 
     // SET DEFAULT CONTROLS
     const ctrls = {
@@ -157,6 +180,7 @@ window.onload = async () => {
     const world = new PIXI.Container()
 
     app.stage.addChild(world)
+    app.stage.sortableChildren = true
 
     app.ticker.add(() => {
         world.x = -camX + cvs.width / 2
@@ -168,8 +192,8 @@ window.onload = async () => {
         color: 0x000000,
         width: 640,
         height: 480,
-        size: 20,
-        lineOffset: -9,
+        size: 22,
+        lineOffset: -8,
         colOffset: -2,
         text: [
             {
@@ -197,38 +221,172 @@ window.onload = async () => {
             },
             "endl",
             {
-                text: "                                          v0.0.1",
+                text: " ".repeat(43 - version.length) + version,
                 color: 0xffffff
             }
         ]
     }
 
-    let testTerm = {
+    let settingsText = {
         color: 0x000000,
-        width: 640,
-        height: 480,
-        size: 20,
-        lineOffset: -9,
+        width: 504, // 36 chars (32)
+        height: 330, // 11 lines (9)
+        size: 22,
+        lineOffset: -1,
         colOffset: -2,
         text: [
             {
-                text: "MAJAVAAA",
+                text: "Master Volume",
+                color: 0x000000,
+                highlight: 0xffffff
+            },
+            {
+                text: "               "
+            },
+            {
+                text: "100%",
                 color: 0xffffff
-            }
+            },
+            "endl",
+            {
+                text: "Controls",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "Fullscreen",
+                color: 0xffffff
+            },
+            {
+                text: "                   "
+            },
+            {
+                text: "OFF",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "",
+                color: 0xffffff
+            },
+            "endl",
+            {
+                text: "Back",
+                color: 0xffffff
+            },
+            "endl"
+        ]
+    }
+
+    const settingsTextIndices = {
+        select: [
+            0,
+            4,
+            6,
+            20
+        ],
+        change: [
+            2,
+            8
         ]
     }
 
     const logoTex = await PIXI.Assets.load(assetsPrefix + "logo.png")
-    logoTex.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
+    sharp(logoTex)
+    
     const logo = new PIXI.Sprite(logoTex)
+    
+    const containers = {}
+    let visible = []
 
-    let mode = "mmStart"
+    function showScreen() {
+        for (const key in containers) {
+            containers[key].visible = false
+        }
 
-    let mmStartSel = 0
+        for (const name of visible) {
+            containers[name].visible = true
+        }
+    }
+
+    // Game State
+    const gs = {
+        mode: "mainmenu",
+        mainmenu: {
+            sel: 0
+        },
+        settings: {
+            shown: false,
+            sel: 0,
+            mode: "main",
+            vol: 100,
+        }
+    }
+    
+
+    const settingsCont = new PIXI.Container()
+    settingsCont.zIndex = 6
+    containers.settings = settingsCont
+
+    const settingsX = 512
+    const settingsY = 338
+
+    settingsCont.pivot.set(settingsX / 2, settingsY / 2)
+    settingsCont.position.set(x / 2, y / 2)
+
+    const settingsBg = await newUiBox(settingsX, settingsY)
+    let settingsTerm = newTerminal(settingsText)
+    settingsTerm.position.set(4, 4)
+    
+    settingsCont.addChild(settingsBg)
+    settingsCont.addChild(settingsTerm)
+    
 
     const mainMenuCont = new PIXI.Container()
+    mainMenuCont.zIndex = 5
+    containers.mainmenu = mainMenuCont
 
-    let term = newTerminal(mainMenuText)
+    let mainMenuTerm = newTerminal(mainMenuText)
+
+    mainMenuTerm.position.set(0, 0)
+    
+    mainMenuCont.addChild(mainMenuTerm)
+    app.stage.addChild(mainMenuCont)
+
+    
+    app.stage.addChild(settingsCont)
+
+    //mainMenuCont.scale.set(0.75)
+    //mainMenuCont.x = 100
+
+    logo.anchor.set(0.5)
+    logo.position.set(x / 2, 65)
+    //mainMenuCont.addChild(logo)
+    logo.scale.set(3)
+
+    function isMode(mode) {
+        return gs.mode === mode && !gs.settings.shown
+    }
 
     function replaceTerminal(oldTerm, newTerm) {
         const parent = oldTerm.parent
@@ -257,60 +415,169 @@ window.onload = async () => {
         return newTerm
     }
 
-    term.x = 0
-    term.y = 0
-    
-    mainMenuCont.addChild(term)
-    app.stage.addChild(mainMenuCont)
+    function updateMode() {
+        visible = []
+        
+        if (gs.mode === "mainmenu") {
+            visible.push("mainmenu")
+            
+            const newText = removeHls(mainMenuText)
+            const newHlText = newText.text[gs.mainmenu.sel * 3]
+            if (typeof newHlText !== "string") {
+                newHlText.highlight = 0xffffff
+                delete newHlText.color
+            }
+            mainMenuTerm = replaceTerminal(mainMenuTerm, newTerminal(newText))
+        }
 
-    logo.anchor.set(0.5)
-    logo.position.set(x / 2, 65)
-    mainMenuCont.addChild(logo)
-    logo.scale.set(3)
+        if (gs.settings.shown) {
+            visible.push("settings")
 
-    addEventListener("keydown", (e) => {
-        if (mode === "mmStart") {
-            if (low(e.key) === ctrls.down) {
-                mmStartSel += 1
-                mmStartSel = clamp(mmStartSel, 0, 3)
-                testTerm.text[0].text = mmStartSel
-                const newText = removeHls(mainMenuText)
-                const newHlText = newText.text[mmStartSel * 3]
+            if (gs.settings.mode === "main") {
+                const newText = removeHls(settingsText)
+                const volText = newText.text[settingsTextIndices.change[0]]
+                const newHlText = newText.text[settingsTextIndices.select[gs.settings.sel]]
                 if (typeof newHlText !== "string") {
                     newHlText.highlight = 0xffffff
                     delete newHlText.color
+                    volText.text = (gs.settings.vol + "%").padStart(4, " ")
                 }
-                term = replaceTerminal(term, newTerminal(newText))
+                settingsTerm = replaceTerminal(settingsTerm, newTerminal(newText))
+                settingsTerm.position.set(4, 4)
+            }
+
+            if (gs.settings.mode === "vol") {
+                const newText = removeHls(settingsText)
+                const newHlText = newText.text[settingsTextIndices.change[0]]
+                if (typeof newHlText !== "string") {
+                    newHlText.highlight = 0xffffff
+                    delete newHlText.color
+                    newHlText.text = (gs.settings.tempVol + "%").padStart(4, " ")
+                }
+                settingsTerm = replaceTerminal(settingsTerm, newTerminal(newText))
+                settingsTerm.position.set(4, 4)
+            }
+        }
+
+        showScreen()
+    }
+
+    updateMode()
+
+    // KEY EVENTS
+
+    addEventListener("keydown", (e) => {
+        if (isMode("mainmenu")) {
+            if (low(e.key) === ctrls.down) {
+                gs.mainmenu.sel += 1
+                gs.mainmenu.sel = clamp(gs.mainmenu.sel, 0, 3)
+                updateMode()
+                return
             }
 
             if (low(e.key) === ctrls.up) {
-                mmStartSel += -1
-                mmStartSel = clamp(mmStartSel, 0, 3)
-                testTerm.text[0].text = mmStartSel
-                const newText = removeHls(mainMenuText)
-                const newHlText = newText.text[mmStartSel * 3]
-                if (typeof newHlText !== "string") {
-                    newHlText.highlight = 0xffffff
-                    delete newHlText.color
-                }
-                term = replaceTerminal(term, newTerminal(newText))
+                gs.mainmenu.sel += -1
+                gs.mainmenu.sel = clamp(gs.mainmenu.sel, 0, 3)
+                updateMode()
+                return
             }
 
             if (low(e.key) === ctrls.confirm) {
-                if (mmStartSel === 0) {
+                if (gs.mainmenu.sel === 0) {
                     alert("PLAY")
-                } else if (mmStartSel === 1) {
-                    alert("SETTINGS")
-                } else if (mmStartSel === 2) {
-                    alert("ACHIEVEMENTS")
-                } else if (mmStartSel === 3) {
+                    return
+                } else if (gs.mainmenu.sel === 1) {
+                    gs.settings.shown = true
+                    updateMode()
+                    return
+                } else if (gs.mainmenu.sel === 2) {
+                    gs.mode = ""
+                    updateMode()
+                    return
+                } else if (gs.mainmenu.sel === 3) {
                     alert("CREDITS")
+                    return
+                }
+            }
+        }
+
+        if (gs.settings.shown) {
+            if (gs.settings.mode === "main") {
+                if (low(e.key) === ctrls.cancel) {
+                    gs.settings.shown = false
+                    gs.settings.sel = 0
+                    updateMode()
+                    return
+                }
+                
+                if (low(e.key) === ctrls.down) {
+                    gs.settings.sel += 1
+                    gs.settings.sel = clamp(gs.settings.sel, 0, settingsTextIndices.select.length - 1)
+                    updateMode()
+                    return
+                }
+
+                if (low(e.key) === ctrls.up) {
+                    gs.settings.sel -= 1
+                    gs.settings.sel = clamp(gs.settings.sel, 0, settingsTextIndices.select.length - 1)
+                    updateMode()
+                    return
+                }
+
+                if (low(e.key) === ctrls.confirm) {
+                    if (gs.settings.sel === 0) {
+                        gs.settings.mode = "vol"
+                        gs.settings.tempVol = gs.settings.vol
+                        updateMode()
+                        return
+                    } else if (gs.settings.sel === 1) {
+                        alert("controls")
+                    } else if (gs.settings.sel === 2) {
+                        alert("fullscreen")
+                    } else if (gs.settings.sel === 3) {
+                        gs.settings.shown = false
+                        gs.settings.sel = 0
+                        updateMode()
+                        return
+                    }
+                }
+            }
+
+            if (gs.settings.mode === "vol") {
+                if (low(e.key) === ctrls.cancel) {
+                    gs.settings.tempVol = gs.settings.vol
+                    gs.settings.mode = "main"
+                    updateMode()
+                    return
+                }
+
+                if (low(e.key) === ctrls.confirm) {
+                    gs.settings.vol = gs.settings.tempVol
+                    gs.settings.mode = "main"
+                    updateMode()
+                    return
+                }
+
+                if (low(e.key) === ctrls.up) {
+                    const volDif = e.shiftKey ? 1 : 10
+                    gs.settings.tempVol += volDif
+                    gs.settings.tempVol = clamp(gs.settings.tempVol, 0, 100)
+                    updateMode()
+                    return
+                }
+
+                if (low(e.key) === ctrls.down) {
+                    const volDif = e.shiftKey ? 1 : 10
+                    gs.settings.tempVol -= volDif
+                    gs.settings.tempVol = clamp(gs.settings.tempVol, 0, 100)
+                    updateMode()
+                    return
                 }
             }
         }
     })
 
     app.ticker.add(() => {
-        camX += 1
+        camX += 0
     })
 }
